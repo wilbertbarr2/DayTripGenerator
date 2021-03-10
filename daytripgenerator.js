9d57b2a6ccb969297511819cf1cc23a2baed8ee3
John Barr <wilbert.barr2@gmail.com>
2021-03-10 16:40:11 -0600

    updated random function

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("hello world");
+
+let destinations = ["London", "Paris", "Tokyo", "Canada"];
+let randomDestination = destinations[Math.floor(Math.random() * destinations.lenght)];
+console.log("test");
+
+let restaurant = ["Apple B's",  "Old country", "Pizza", "SteakOut"];
+let randRestaurant = restaurant[Math.floor(Math.round() * restaurant.lenght)];
+console.log("test");
+
+let transportation = ["Plane", "Car" , "Train" , "Bus" ];
+let randomTranspot = transportation[Math.floor(Math.round() * transportation.length)];
+console.log("test");
+
+let entertainment = ["Snowboarding", "Hiking", "Walking", "Movies"];
+let randEntainment = entertainment[Math.floor(Math.round() * entertainment.length)];
+console.log("test");
+
+let fullTrip = [("destination.lenght")  +  ("trasportation.lenght")  +  ("entertainment.lenght")];
+console.log("test");
